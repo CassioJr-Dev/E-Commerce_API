a0c085292883257d3ca89df9394904b0a5c05a7b
Cassio <cassiojr0108@gmail.com>
2024-03-11 15:21:43 -0300

    format code and uuid entity

--- src/shared/infrastructure/env-config/__tests__/unit/env-config.service.spec.ts
+++ src/shared/infrastructure/env-config/__tests__/unit/env-config.service.spec.ts
@@ -3,22 +3,22 @@ import { EnvConfigService } from '../../env-config.service';
 import { EnvConfigModule } from '../../env-config.module';
 
 describe('EnvConfigService unit tests', () => {
-    let sut: EnvConfigService;
+  let sut: EnvConfigService;
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            imports: [EnvConfigModule.forRoot()],
-            providers: [EnvConfigService],
-        }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [EnvConfigModule.forRoot()],
+      providers: [EnvConfigService],
+    }).compile();
 
-        sut = module.get<EnvConfigService>(EnvConfigService);
-    });
+    sut = module.get<EnvConfigService>(EnvConfigService);
+  });
 
-    it('should be defined', () => {
-        expect(sut).toBeDefined();
-    });
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+  });
 
-    it('should return the variable PORT', () => {
-        expect(sut.getAppPort()).toBe(3000);
-    });
+  it('should return the variable PORT', () => {
+    expect(sut.getAppPort()).toBe(3000);
+  });
 });
--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -4,17 +4,17 @@ import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 import { join } from 'node:path';
 
 @Module({
-    imports: [ConfigModule],
-    providers: [EnvConfigService],
-    exports: [EnvConfigService]
+  imports: [ConfigModule],
+  providers: [EnvConfigService],
+  exports: [EnvConfigService],
 })
 export class EnvConfigModule extends ConfigModule {
-    static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
-        return super.forRoot({
-            ...options,
-            // envFilePath: [
-            //     join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
-            // ],
-        });
-    }
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return super.forRoot({
+      ...options,
+      // envFilePath: [
+      //     join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
+      // ],
+    });
+  }
 }
