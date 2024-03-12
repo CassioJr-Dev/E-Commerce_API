5e0452fbe44086306d4f3a07ad619c3636966bbf
Cassio <cassiojr0108@gmail.com>
2024-03-12 16:56:07 -0300

    Refactoring of the toJson method

--- src/shared/infrastructure/domain/entities/entity.ts
+++ src/shared/infrastructure/domain/entities/entity.ts
@@ -3,25 +3,37 @@ import { v4 as uuidv4 } from 'uuid';
 export abstract class Entity<Props = any> {
   public readonly _id: string;
   public readonly props: Props;
-  public readonly created_at: Date;
-  public readonly update_at: Date;
+  public readonly _created_at: Date;
+  public readonly _update_at: Date;
 
   constructor(props: Props, id?: string, created_at?: Date, update_at?: Date) {
     this.props = props;
     this._id = id || uuidv4();
-    this.created_at = created_at || this.toDate();
-    this.update_at = update_at || this.toDate()
+    this._created_at = created_at || this.toDate();
+    this._update_at = update_at || this.toDate()
   }
 
   get id() {
     return this._id;
   }
 
-  toJSON(): Required<{ id: string } & Props> {
+  get created_at() {
+    return this._created_at;
+  }
+
+  get update_at() {
+    return this._update_at;
+  }
+
+  toJSON(): Required<{ id: string, created_at: Date, update_at: Date } & Props> {
+
     return {
       id: this._id,
       ...this.props,
-    } as Required<{ id: string } & Props>;
+      created_at: this._created_at,
+      update_at: this._update_at
+    } as Required<{ id: string, created_at: Date, update_at: Date } & Props>;
+
   }
 
   toDate(): Date {
