7c0e868a6382b7211703da961c3f5fc05d177adf
Cassio <cassiojr0108@gmail.com>
2024-03-12 11:51:09 -0300

    Adding created_at and update_at property in Entity class

--- src/shared/infrastructure/domain/entities/entity.ts
+++ src/shared/infrastructure/domain/entities/entity.ts
@@ -3,10 +3,14 @@ import { v4 as uuidv4 } from 'uuid';
 export abstract class Entity<Props = any> {
   public readonly _id: string;
   public readonly props: Props;
+  public readonly created_at: Date;
+  public readonly update_at: Date;
 
-  constructor(props: Props, id?: string) {
+  constructor(props: Props, id?: string, created_at?: Date, update_at?: Date) {
     this.props = props;
     this._id = id || uuidv4();
+    this.created_at = created_at || this.toDate();
+    this.update_at = update_at || this.toDate()
   }
 
   get id() {
@@ -18,5 +22,12 @@ export abstract class Entity<Props = any> {
       id: this._id,
       ...this.props,
     } as Required<{ id: string } & Props>;
+  }
+
+  toDate(): Date {
+    const newDate = new Date()
+    newDate.setUTCHours(newDate.getUTCHours() - 3);
+
+    return newDate;
   }
 }
