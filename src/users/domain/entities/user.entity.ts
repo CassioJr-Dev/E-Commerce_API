b145c5cc7f585d68af22b513a249d718961d5fde
Cassio <cassiojr0108@gmail.com>
2024-03-15 11:30:29 -0300

    Created UserEntity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { Entity } from "@/shared/infrastructure/domain/entities/entity"
 
 export type UserProps = {
   name: string
@@ -5,4 +6,26 @@ export type UserProps = {
   email: string
   password: string
 }
-export class User {}
+
+export class UserEntity extends Entity<UserProps>{
+  constructor(public readonly props: UserProps, id?:string, created_at?: Date, update_at?: Date ) {
+    super(props, id, created_at, update_at)
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  get isSeller() {
+    return this.props.isSeller
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+}
