37b34ad30bedf25c8e06bb021d1345d05ad2e048
Cassio <cassiojr0108@gmail.com>
2024-03-15 12:04:42 -0300

    Created User data builder

---
+++ src/users/domain/testing/helpers/user-data-builder.ts
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker'
+import { UserProps } from "../../entities/user.entity"
+
+type Props = {
+  name?: string
+  isSeller?: boolean
+  email?: string
+  password?: string
+}
+
+export function UserDataBuilder(props: Props): UserProps {
+  return {
+    name: props.name ?? faker.person.fullName(),
+    isSeller: props.isSeller ?? false,
+    email: props.email ?? faker.internet.email(),
+    password: props.password ?? faker.internet.password()
+  }
+}
