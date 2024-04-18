8108a4e534e2d684c3d5a2ff379e3b4f1d7ee377
Cassio <cassiojr0108@gmail.com>
2024-04-18 11:40:53 -0300

    ListUsersUseCase unit tests

---
+++ src/users/application/usecases/__tests__/unit/listusers.usecase.spec.ts
@@ -0,0 +1,130 @@
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/repositories/user-in-memory.repository';
+import { ListUserUseCase } from '../../listusers.usecase';
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+
+describe('ListUsersUseCase unit tests', () => {
+  let sut: ListUserUseCase.UseCase;
+  let repository: UserInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository();
+    sut = new ListUserUseCase.UseCase(repository);
+  });
+
+  it('toOutput method', () => {
+    let result = new UserRepository.SearchResult({
+      items: [],
+      total: 1,
+      currentPage: 1,
+      perPage: 2,
+      sort: null,
+      sortDir: null,
+      filter: null,
+    });
+    let output = sut['toOutput'](result);
+    expect(output).toStrictEqual({
+      items: [],
+      total: 1,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 2,
+    });
+
+    const entity = new UserEntity(UserDataBuilder({}));
+    result = new UserRepository.SearchResult({
+      items: [entity],
+      total: 1,
+      currentPage: 1,
+      perPage: 2,
+      sort: null,
+      sortDir: null,
+      filter: null,
+    });
+    output = sut['toOutput'](result);
+    expect(output).toStrictEqual({
+      items: [entity.toJSON()],
+      total: 1,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 2,
+    });
+  });
+
+  it('should return the users ordered by created_at', async () => {
+    const created_at = new Date();
+    const items = [
+      new UserEntity(UserDataBuilder({}), undefined, created_at),
+      new UserEntity(
+        UserDataBuilder({}),
+        undefined,
+        new Date(created_at.getTime() + 1),
+      ),
+    ];
+    repository.items = items;
+    const output = await sut.execute({});
+    expect(output).toStrictEqual({
+      items: [...items].reverse().map(item => item.toJSON()),
+      total: 2,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 15,
+    });
+  });
+
+  it('should return the users using pagination, sort and filter', async () => {
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'a' })),
+      new UserEntity(UserDataBuilder({ name: 'AA' })),
+      new UserEntity(UserDataBuilder({ name: 'Aa' })),
+      new UserEntity(UserDataBuilder({ name: 'b' })),
+      new UserEntity(UserDataBuilder({ name: 'c' })),
+    ];
+    repository.items = items;
+    let output = await sut.execute({
+      page: 1,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'a',
+    });
+    expect(output).toStrictEqual({
+      items: [items[1].toJSON(), items[2].toJSON()],
+      total: 3,
+      currentPage: 1,
+      lastPage: 2,
+      perPage: 2,
+    });
+
+    output = await sut.execute({
+      page: 2,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'a',
+    });
+    expect(output).toStrictEqual({
+      items: [items[0].toJSON()],
+      total: 3,
+      currentPage: 2,
+      lastPage: 2,
+      perPage: 2,
+    });
+
+    output = await sut.execute({
+      page: 1,
+      perPage: 3,
+      sort: 'name',
+      sortDir: 'desc',
+      filter: 'a',
+    });
+    expect(output).toStrictEqual({
+      items: [items[0].toJSON(), items[2].toJSON(), items[1].toJSON()],
+      total: 3,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 3,
+    });
+  });
+});
