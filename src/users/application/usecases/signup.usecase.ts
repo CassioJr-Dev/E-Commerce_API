95a920b8558f925915ddfda769610f5109efbb6e
Cassio <cassiojr0108@gmail.com>
2024-04-19 09:36:21 -0300

    Refactory returning of usecases

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -2,7 +2,7 @@ import { HashProvider } from '@/shared/application/providers/hash-provider';
 import { BadRequestError } from '@/shared/domain/errors/bad-request-error';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserRepository } from '@/users/domain/repositories/user.repository';
-import { UserOutput } from '../dtos/user-output';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output';
 import { UseCase as DefaultUseCase } from './use-case';
 
 export namespace SignupUseCase {
@@ -39,7 +39,7 @@ export namespace SignupUseCase {
 
       await this.userRepository.insert(entity);
 
-      return entity.toJSON();
+      return UserOutputMapper.toOutput(entity);
     }
   }
 }
