98f6d056841b3af45b7334ed96e4c482df1415e3
Cassio <cassiojr0108@gmail.com>
2024-04-19 11:12:24 -0300

    UpdateUserUseCase unit tests

---
+++ src/users/application/usecases/__tests__/unit/update-user.usecase.spec.ts
@@ -0,0 +1,53 @@
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/repositories/user-in-memory.repository';
+import { UpdateUserUseCase } from '../../update-user.usecase';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { BadRequestError } from '@/shared/domain/errors/bad-request-error';
+
+describe('UpdateUserUseCase unit tests', () => {
+  let sut: UpdateUserUseCase.UseCase;
+  let repository: UserInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository();
+    sut = new UpdateUserUseCase.UseCase(repository);
+  });
+
+  it('Should throws error when entity not found', async () => {
+    await expect(() =>
+      sut.execute({ id: 'fake', name: 'test name' }),
+    ).rejects.toThrow(new NotFoundError('Entity not found'));
+  });
+
+  it('Should generate error when no properties are provided', async () => {
+    repository.items = [new UserEntity(UserDataBuilder({}))];
+    await expect(() =>
+      sut.execute({ id: repository.items[0].id }),
+    ).rejects.toThrow(new BadRequestError('No valid properties provided'));
+  });
+
+  it('Should update a user', async () => {
+    const spyUpdate = jest.spyOn(repository, 'update');
+    const items = [new UserEntity(UserDataBuilder({}))];
+    repository.items = items;
+
+    const result = await sut.execute({
+      id: items[0].id,
+      name: 'new name',
+      isSeller: true,
+      email: 'newemail@gmail.com',
+    });
+
+    expect(spyUpdate).toHaveBeenCalledTimes(1);
+    expect(result).toMatchObject({
+      id: items[0].id,
+      name: 'new name',
+      isSeller: true,
+      email: 'newemail@gmail.com',
+      created_at: items[0].created_at,
+      update_at: items[0].update_at,
+    });
+    console.log(result);
+  });
+});
