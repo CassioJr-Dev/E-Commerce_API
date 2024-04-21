9c1fadacccc88a3af6ea7383cf19ed6931bb4279
Cassio <cassiojr0108@gmail.com>
2024-04-21 16:06:08 -0300

    Adding SigninUseCase unit tests

--- src/users/application/usecases/signin.usecase.ts
+++ src/users/application/usecases/signin.usecase.ts
@@ -1,43 +1,42 @@
-import { UserRepository } from "@/users/domain/repositories/user.repository"
-import { UserOutput, UserOutputMapper } from "../dtos/user-output"
-import { UseCase as DefaultUseCase } from "@/shared/application/usecases/use-case"
-import { HashProvider } from "@/shared/application/providers/hash-provider"
-import { BadRequestError } from "@/shared/domain/errors/bad-request-error"
-import { InvalidCredentialsError } from "@/shared/domain/errors/invalid-credentials-error"
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output';
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+import { BadRequestError } from '@/shared/domain/errors/bad-request-error';
+import { InvalidCredentialsError } from '@/shared/domain/errors/invalid-credentials-error';
 export namespace SigninUseCase {
   export type Input = {
-    email: string
-    password: string
-  }
+    email: string;
+    password: string;
+  };
 
-  export type Output = UserOutput
+  export type Output = UserOutput;
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(
       private userRepository: UserRepository.Repository,
       private hashProvider: HashProvider,
-    ){}
+    ) {}
 
     async execute(input: Input) {
-      const { email, password } = input
+      const { email, password } = input;
 
-      if(!email || !password) {
-        throw new BadRequestError('Input data not provided')
+      if (!email || !password) {
+        throw new BadRequestError('Input data not provided');
       }
 
-      const entity = await this.userRepository.findByEmail(email)
+      const entity = await this.userRepository.findByEmail(email);
 
       const hashPasswordMatches = await this.hashProvider.compareHash(
         password,
-        entity.password
-      )
+        entity.password,
+      );
 
-      if(!hashPasswordMatches) {
-        throw new InvalidCredentialsError('Invalid credentials')
+      if (!hashPasswordMatches) {
+        throw new InvalidCredentialsError('Invalid credentials');
       }
 
-      return UserOutputMapper.toOutput(entity)
+      return UserOutputMapper.toOutput(entity);
     }
-
   }
 }
