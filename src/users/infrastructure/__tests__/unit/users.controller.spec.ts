04c829ca2f95e936190b5194d72eb9b8cbeadaf3
Cassio <cassiojr0108@gmail.com>
2024-04-24 11:01:22 -0300

    UsersController unit tests - login method

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -2,6 +2,8 @@ import { UserOutput } from '@/users/application/dtos/user-output';
 import { UsersController } from '../../users.controller';
 import { SignupUseCase } from '@/users/application/usecases/signup.usecase';
 import { SignupDto } from '../../dtos/signup.dto';
+import { SigninUseCase } from '@/users/application/usecases/signin.usecase';
+import { SigninDto } from '../../dtos/signin.dto';
 
 describe('UsersController unit tests', () => {
   let sut: UsersController;
@@ -42,5 +44,24 @@ describe('UsersController unit tests', () => {
 
     expect(output).toMatchObject(result);
     expect(mockSignupUseCase.execute).toHaveBeenCalledWith(input);
+  });
+
+  it('Should authenticate a user', async () => {
+    const output: SigninUseCase.Output = props;
+    const mockSigninUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+
+    sut['signinUseCase'] = mockSigninUseCase as any;
+
+    const input: SigninDto = {
+      email: 'a@a.com',
+      password: '1234',
+    };
+
+    const result = await sut.login(input);
+
+    expect(output).toMatchObject(result);
+    expect(mockSigninUseCase.execute).toHaveBeenCalledWith(input);
   });
 });
