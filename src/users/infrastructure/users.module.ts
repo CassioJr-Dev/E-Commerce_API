510dd60fad3d9b4b0c55a95bcebc397b53adcabe
Cassio <cassiojr0108@gmail.com>
2024-04-24 10:06:36 -0300

    Configuring UsersController

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository';
 import { BcryptjsHashProvider } from './providers/hash-provider/bcryptjs-hash.provider';
@@ -16,7 +15,6 @@ import { DeleteUserUseCase } from '../application/usecases/delete-user.usecase';
 @Module({
   controllers: [UsersController],
   providers: [
-    UsersService,
     {
       provide: 'UserRepository',
       useClass: UserInMemoryRepository,
