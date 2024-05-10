6e983bd50686679977661fa4857648915b865af0
Cassio <cassiojr0108@gmail.com>
2024-05-10 20:49:06 -0300

    Adding SignupUseCase integration tests

---
+++ src/users/application/usecases/__tests__/integration/signup.usecase.int-spec.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client"
+import { SignupUseCase } from "../../signup.usecase"
+import { UserPrismaRepository } from "@/users/infrastructure/database/prisma/repositories/user-prisma.repository"
+import { Test, TestingModule } from "@nestjs/testing"
+import { HashProvider } from "@/shared/application/providers/hash-provider"
+import { setupPrismaTests } from "@/users/infrastructure/database/prisma/testing/setup-prisma-tests"
+import { DatabaseModule } from "@/shared/infrastructure/database/database.module"
+import { BcryptjsHashProvider } from "@/users/infrastructure/providers/hash-provider/bcryptjs-hash.provider"
+import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
+
+describe('SignupUseCase integration tests', () => {
+  const prismaService = new PrismaClient()
+  let sut: SignupUseCase.UseCase
+  let repository: UserPrismaRepository
+  let module: TestingModule
+  let hashProvider: HashProvider
+
+  beforeAll(async () => {
+    setupPrismaTests()
+    module = await Test.createTestingModule({
+      imports: [DatabaseModule.forTest(prismaService)]
+    }).compile()
+
+    repository = new UserPrismaRepository(prismaService as any)
+    hashProvider = new BcryptjsHashProvider()
+
+  })
+
+  beforeEach(async () => {
+    sut = new SignupUseCase.UseCase(repository, hashProvider)
+    await prismaService.user.deleteMany()
+  })
+
+  afterAll(async () => {
+    await prismaService.user.deleteMany()
+    module.close
+  })
+
+  it('Should create a user', async () => {
+    const props = {
+      ...UserDataBuilder({})
+    }
+    const output = await sut.execute(props)
+    expect(output.id).toBeDefined()
+    expect(output.created_at).toBeInstanceOf(Date)
+    expect(output.updated_at).toBeInstanceOf(Date)
+  })
+})
