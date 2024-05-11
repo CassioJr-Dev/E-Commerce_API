776e333139b53e3f19b675e67f4f85e36d8dcacd
Cassio <cassiojr0108@gmail.com>
2024-05-10 22:06:14 -0300

    Adding SigninUseCase integration tests

---
+++ src/users/application/usecases/__tests__/integration/signin.usecase.int-spec.ts
@@ -0,0 +1,104 @@
+import { PrismaClient } from '@prisma/client';
+import { SigninUseCase } from '../../signin.usecase';
+import { UserPrismaRepository } from '@/users/infrastructure/database/prisma/repositories/user-prisma.repository';
+import { Test, TestingModule } from '@nestjs/testing';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+import { setupPrismaTests } from '@/users/infrastructure/database/prisma/testing/setup-prisma-tests';
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
+import { BcryptjsHashProvider } from '@/users/infrastructure/providers/hash-provider/bcryptjs-hash.provider';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
+import { InvalidCredentialsError } from '@/shared/domain/errors/invalid-credentials-error';
+import { BadRequestError } from '@/shared/domain/errors/bad-request-error';
+
+describe('SigninUseCase integration tests', () => {
+  const prismaService = new PrismaClient();
+  let sut: SigninUseCase.UseCase;
+  let repository: UserPrismaRepository;
+  let module: TestingModule;
+  let hashProvider: HashProvider;
+
+  beforeAll(async () => {
+    setupPrismaTests();
+    module = await Test.createTestingModule({
+      imports: [DatabaseModule.forTest(prismaService)],
+    }).compile();
+    repository = new UserPrismaRepository(prismaService as any);
+    hashProvider = new BcryptjsHashProvider();
+  });
+
+  beforeEach(async () => {
+    sut = new SigninUseCase.UseCase(repository, hashProvider);
+    await prismaService.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
+  it('Should not be able to authenticate with wrong email', async () => {
+    const entity = new UserEntity(UserDataBuilder({}));
+    await expect(() =>
+      sut.execute({
+        email: entity.email,
+        password: '1234',
+      }),
+    ).rejects.toBeInstanceOf(NotFoundError);
+  });
+
+  it('Should not be able to authenticate with wrong password', async () => {
+    const hashPassword = await hashProvider.generateHash('1234');
+    const entity = new UserEntity(
+      UserDataBuilder({
+        email: 'a@a.com',
+        password: hashPassword,
+      }),
+    );
+    await prismaService.user.create({
+      data: entity.toJSON(),
+    });
+
+    await expect(() =>
+      sut.execute({
+        email: 'a@a.com',
+        password: 'fake',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+
+  it('Should throws error when email not provided', async () => {
+    await expect(() =>
+      sut.execute({
+        email: null,
+        password: '1234',
+      }),
+    ).rejects.toBeInstanceOf(BadRequestError);
+  });
+
+  it('Should throws error when password not provided', async () => {
+    await expect(() =>
+      sut.execute({
+        email: 'a@a.com',
+        password: null,
+      }),
+    ).rejects.toBeInstanceOf(BadRequestError);
+  });
+
+  it('Should authenticate a user', async () => {
+    const hashPassword = await hashProvider.generateHash('1234');
+    const entity = new UserEntity(
+      UserDataBuilder({ email: 'a@a.com', password: hashPassword }),
+    );
+    await prismaService.user.create({
+      data: entity.toJSON(),
+    });
+
+    const output = await sut.execute({
+      email: 'a@a.com',
+      password: '1234',
+    });
+
+    expect(output).toMatchObject(entity.toJSON());
+  });
+});
