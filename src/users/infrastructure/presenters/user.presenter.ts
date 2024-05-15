5db9fc72707b8cfd94b7e2b9dd46f3dd2fe2396b
Cassio <cassiojr0108@gmail.com>
2024-05-15 09:29:51 -0300

    Adding UserPresenter class

---
+++ src/users/infrastructure/presenters/user.presenter.ts
@@ -0,0 +1,21 @@
+import { UserOutput } from '@/users/application/dtos/user-output';
+import { Transform } from 'class-transformer';
+
+export class UserPresenter {
+  id: string;
+  name: string;
+  isSeller: boolean;
+  email: string;
+  @Transform(({ value }: { value: Date }) => value.toISOString())
+  created_at: Date;
+  @Transform(({ value }: { value: Date }) => value.toISOString())
+  updated_at: Date;
+
+  constructor(output: UserOutput) {
+    this.id = output.id;
+    this.name = output.name;
+    this.isSeller = output.isSeller;
+    this.email = output.email;
+    this.updated_at = output.updated_at;
+  }
+}
