67e3eed3339263b5e6547c88177ebc22ce121f19
Cassio <cassiojr0108@gmail.com>
2024-05-23 20:36:22 -0300

    Config ValidationPipe and DTOs

--- src/users/infrastructure/dtos/signup.dto.ts
+++ src/users/infrastructure/dtos/signup.dto.ts
@@ -1,8 +1,21 @@
 import { SignupUseCase } from '@/users/application/usecases/signup.usecase';
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignupDto implements SignupUseCase.Input {
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsBoolean()
   isSeller?: boolean;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
--- src/users/infrastructure/dtos/update-user.dto.ts
+++ src/users/infrastructure/dtos/update-user.dto.ts
@@ -1,7 +1,14 @@
 import { UpdateUserUseCase } from '@/users/application/usecases/update-user.usecase';
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateUserDto implements Omit<UpdateUserUseCase.Input, 'id'> {
+
+  @IsString()
   name?: string;
+
+  @IsBoolean()
   isSeller?: boolean;
+
+  @IsEmail()
   email?: string;
 }
