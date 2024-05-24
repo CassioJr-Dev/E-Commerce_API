52458b294153aaed20ad087c66298ccd68af8320
Cassio <cassiojr0108@gmail.com>
2024-05-24 16:42:48 -0300

    NotFoundErrorFilter e2e tests

--- src/shared/infrastructure/exception-filters/not-found-error/__tests__/e2e/not-found-error.filter.e2e-spec.ts
+++ src/shared/infrastructure/exception-filters/not-found-error/__tests__/e2e/not-found-error.filter.e2e-spec.ts
@@ -1,7 +1,39 @@
+import { Controller, Get, INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
 import { NotFoundErrorFilter } from '../../not-found-error.filter';
+import request from 'supertest';
 
-describe('NotFoundErrorFilter', () => {
-  it('Should be defined', () => {
+@Controller('stub')
+class StubController {
+  @Get()
+  index() {
+    throw new NotFoundError('UserModel not found');
+  }
+}
+
+describe('NotFoundErrorFilter (e2e)', () => {
+  let app: INestApplication;
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      controllers: [StubController],
+    }).compile();
+    app = module.createNestApplication();
+    app.useGlobalFilters(new NotFoundErrorFilter());
+    await app.init();
+  });
+
+  it('should be defined', () => {
     expect(new NotFoundErrorFilter()).toBeDefined();
+  });
+
+  it('should catch a NotFoundError', () => {
+    return request(app.getHttpServer()).get('/stub').expect(404).expect({
+      statusCode: 404,
+      error: 'Not Found',
+      message: 'UserModel not found',
+    });
   });
 });
