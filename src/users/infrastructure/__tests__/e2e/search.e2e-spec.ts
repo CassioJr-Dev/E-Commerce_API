14bb7e1fc38259e3aabe00fbf8d628e9e5468a04
Cassio <cassiojr0108@gmail.com>
2024-05-24 17:09:27 -0300

    search.e2e format

--- src/users/infrastructure/__tests__/e2e/search.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/search.e2e-spec.ts
@@ -1,66 +1,70 @@
-import { UserRepository } from "@/users/domain/repositories/user.repository"
-import { INestApplication } from "@nestjs/common"
-import { Test, TestingModule } from "@nestjs/testing"
-import { PrismaClient } from "@prisma/client"
-import { setupPrismaTests } from "../../database/prisma/testing/setup-prisma-tests"
-import { EnvConfigModule } from "@/shared/infrastructure/env-config/env-config.module"
-import { DatabaseModule } from "@/shared/infrastructure/database/database.module"
-import { UsersModule } from "../../users.module"
-import { applyGlobalConfig } from "@/global-config"
-import { UserEntity } from "@/users/domain/entities/user.entity"
-import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { setupPrismaTests } from '../../database/prisma/testing/setup-prisma-tests';
+import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module';
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
+import { UsersModule } from '../../users.module';
+import { applyGlobalConfig } from '@/global-config';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
 import request from 'supertest';
-import { instanceToPlain } from "class-transformer"
-import { UsersController } from "../../users.controller"
+import { instanceToPlain } from 'class-transformer';
+import { UsersController } from '../../users.controller';
 
 describe('UsersController e2e tests', () => {
-  let app: INestApplication
-  let module: TestingModule
-  let repository: UserRepository.Repository
-  const prismaService = new PrismaClient()
+  let app: INestApplication;
+  let module: TestingModule;
+  let repository: UserRepository.Repository;
+  const prismaService = new PrismaClient();
 
   beforeAll(async () => {
-    setupPrismaTests()
+    setupPrismaTests();
     module = await Test.createTestingModule({
       imports: [
         EnvConfigModule,
         UsersModule,
         DatabaseModule.forTest(prismaService),
       ],
-    }).compile()
-    app = module.createNestApplication()
-    applyGlobalConfig(app)
-    await app.init()
-    repository = module.get<UserRepository.Repository>('UserRepository')
-  })
+    }).compile();
+    app = module.createNestApplication();
+    applyGlobalConfig(app);
+    await app.init();
+    repository = module.get<UserRepository.Repository>('UserRepository');
+  });
 
   beforeEach(async () => {
-    await prismaService.user.deleteMany()
-  })
+    await prismaService.user.deleteMany();
+  });
 
   describe('GET /users', () => {
     it('Should return the users ordered by created_at', async () => {
-      const created_at = new Date()
-      const entities: UserEntity[] = []
-      const arrange = Array(3).fill(UserDataBuilder({}))
+      const created_at = new Date();
+      const entities: UserEntity[] = [];
+      const arrange = Array(3).fill(UserDataBuilder({}));
       arrange.forEach((element, index) => {
         entities.push(
-          new UserEntity({
-            ...element,
-            email: `a${index}@a.com`,
-          }, undefined, new Date(created_at.getTime() + index)),
-        )
-      })
+          new UserEntity(
+            {
+              ...element,
+              email: `a${index}@a.com`,
+            },
+            undefined,
+            new Date(created_at.getTime() + index),
+          ),
+        );
+      });
       await prismaService.user.createMany({
         data: entities.map(item => item.toJSON()),
-      })
-      const searchParams = {}
-      const queryParams = new URLSearchParams(searchParams as any).toString()
+      });
+      const searchParams = {};
+      const queryParams = new URLSearchParams(searchParams as any).toString();
 
       const res = await request(app.getHttpServer())
         .get(`/users/?${queryParams}`)
-        .expect(200)
-      expect(Object.keys(res.body)).toStrictEqual(['data', 'meta'])
+        .expect(200);
+      expect(Object.keys(res.body)).toStrictEqual(['data', 'meta']);
       expect(res.body).toStrictEqual({
         data: [...entities]
           .reverse()
@@ -71,12 +75,12 @@ describe('UsersController e2e tests', () => {
           perPage: 15,
           lastPage: 1,
         },
-      })
-    })
+      });
+    });
 
     it('Should return the users ordered by created_at', async () => {
-      const entities: UserEntity[] = []
-      const arrange = ['test', 'a', 'TEST', 'b', 'TeSt']
+      const entities: UserEntity[] = [];
+      const arrange = ['test', 'a', 'TEST', 'b', 'TeSt'];
       arrange.forEach((element, index) => {
         entities.push(
           new UserEntity({
@@ -84,24 +88,24 @@ describe('UsersController e2e tests', () => {
             name: element,
             email: `a${index}@a.com`,
           }),
-        )
-      })
+        );
+      });
       await prismaService.user.createMany({
         data: entities.map(item => item.toJSON()),
-      })
+      });
       const searchParams = {
         page: 1,
         perPage: 2,
         sort: 'name',
         sortDir: 'asc',
         filter: 'TEST',
-      }
-      const queryParams = new URLSearchParams(searchParams as any).toString()
+      };
+      const queryParams = new URLSearchParams(searchParams as any).toString();
 
       const res = await request(app.getHttpServer())
         .get(`/users/?${queryParams}`)
-        .expect(200)
-      expect(Object.keys(res.body)).toStrictEqual(['data', 'meta'])
+        .expect(200);
+      expect(Object.keys(res.body)).toStrictEqual(['data', 'meta']);
       expect(res.body).toStrictEqual({
         data: [entities[0], entities[4]].map(item =>
           instanceToPlain(UsersController.userToResponse(item)),
@@ -112,16 +116,15 @@ describe('UsersController e2e tests', () => {
           perPage: 2,
           lastPage: 2,
         },
-      })
-    })
+      });
+    });
 
     it('Should return a error with 422 code when the query params is invalid', async () => {
       const res = await request(app.getHttpServer())
         .get('/users/?fakeId=10')
-        .expect(422)
-      expect(res.body.error).toBe('Unprocessable Entity')
-      expect(res.body.message).toEqual(['property fakeId should not exist'])
-    })
-  })
-})
-
+        .expect(422);
+      expect(res.body.error).toBe('Unprocessable Entity');
+      expect(res.body.message).toEqual(['property fakeId should not exist']);
+    });
+  });
+});
