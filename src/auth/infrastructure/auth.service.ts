72f90c83b2169ebd103aabb66a9c394ebd234847
Cassio <cassiojr0108@gmail.com>
2024-05-27 17:28:53 -0300

    AuthService unit tests

--- src/auth/infrastructure/auth.service.ts
+++ src/auth/infrastructure/auth.service.ts
@@ -3,24 +3,24 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
 type GenerateJwtProps = {
-  accessToken: string
-}
+  accessToken: string;
+};
 
 @Injectable()
 export class AuthService {
   constructor(
     private jwtService: JwtService,
-    private configService: EnvConfigService
+    private configService: EnvConfigService,
   ) {}
 
   async generateJwt(userId: string): Promise<GenerateJwtProps> {
-    const accessToken = await this.jwtService.signAsync({ id: userId }, {})
-    return { accessToken }
+    const accessToken = await this.jwtService.signAsync({ id: userId }, {});
+    return { accessToken };
   }
 
   async verifyJwt(token: string) {
     return this.jwtService.verifyAsync(token, {
-      secret: this.configService.getJwtSecret()
-    })
+      secret: this.configService.getJwtSecret(),
+    });
   }
 }
