43e11a08b364df0c11d5a32ed79aaee3d7451780
Cassio <cassiojr0108@gmail.com>
2024-06-05 10:35:10 -0300

    Create product data builder

---
+++ src/products/domain/testing/helpers/product-data-builder.ts
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker';
+import { ProductProps } from '../../entities/product.entity';
+import { randomUUID } from 'node:crypto';
+
+type Props = {
+  name?: string;
+  description?: string;
+  price?: number;
+  user_id?: string;
+};
+
+export function UserDataBuilder(props: Props): ProductProps {
+  return {
+    name: props.name ?? faker.commerce.product(),
+    description: props.description ?? 'description of product',
+    price: props.price ?? Number(faker.finance.amount()),
+    user_id: props.user_id ?? randomUUID(),
+  };
+}
+
