3d0ce000d9ef6cf5dec4d68e9f78ee8a20cfd202
Cassio <cassiojr0108@gmail.com>
2024-06-05 09:40:37 -0300

    Property is Optional

--- src/users/infrastructure/dtos/update-user.dto.ts
+++ src/users/infrastructure/dtos/update-user.dto.ts
@@ -1,17 +1,20 @@
 import { UpdateUserUseCase } from '@/users/application/usecases/update-user.usecase';
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto implements Omit<UpdateUserUseCase.Input, 'id'> {
   @ApiPropertyOptional({ description: 'Nome do usuário' })
+  @IsOptional()
   @IsString()
   name?: string;
 
   @ApiPropertyOptional({ description: 'Usuário é vendedor ou não' })
+  @IsOptional()
   @IsBoolean()
   isSeller?: boolean;
 
   @ApiPropertyOptional({ description: 'E-mail do usuário' })
+  @IsOptional()
   @IsEmail()
   email?: string;
 }
