9fdfdb945e8ab9ed6dd030428188db2b1f007382
Cassio <cassiojr0108@gmail.com>
2024-06-06 20:14:33 -0300

    Correction

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -110,8 +110,6 @@ export class UsersController {
       ...tokenJwt,
       ...UsersController.userToResponse(output),
     };
-
-    // return UsersController.userToResponse(output);
   }
 
   @ApiResponse({
