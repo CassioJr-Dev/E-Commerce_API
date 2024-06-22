b4e61479a393bb482b80330fa40c9027d27f4af5
Cassio <cassiojr0108@gmail.com>
2024-06-22 17:40:08 -0300

    Adding Product Presenter

---
+++ src/products/infrastructure/presenters/product.presenter.ts
@@ -0,0 +1,37 @@
+import { ProductOutput } from '@/products/application/dtos/product-output';
+import { ListProductsUseCase } from '@/products/application/usecases/list-products.usecase';
+import { CollectionPresenter } from '@/shared/infrastructure/presenters/collection.presenter';
+import { Transform } from 'class-transformer';
+
+export class ProductPresenter {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  user_id: string;
+  @Transform(({ value }: { value: Date }) => value.toISOString())
+  created_at: Date;
+  @Transform(({ value }: { value: Date }) => value.toISOString())
+  updated_at: Date;
+
+  constructor(output: ProductOutput) {
+    this.id = output.id;
+    this.name = output.name;
+    this.description = output.description;
+    this.price = output.price;
+    this.stock = output.stock;
+    this.created_at = output.created_at;
+    this.updated_at = output.updated_at;
+  }
+}
+
+export class ProductCollectionPresenter extends CollectionPresenter {
+  data: ProductPresenter[];
+
+  constructor(output: ListProductsUseCase.Output) {
+    const { items, ...paginationProps } = output;
+    super(paginationProps);
+    this.data = items.map(item => new ProductPresenter(item));
+  }
+}
