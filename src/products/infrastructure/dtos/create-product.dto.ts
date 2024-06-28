f7cd7cfbe420976aba787a2c37f48b4b5558e876
Cassio <cassiojr0108@gmail.com>
2024-06-28 00:05:57 -0300

    Documentation of products with swagger

--- src/products/infrastructure/dtos/create-product.dto.ts
+++ src/products/infrastructure/dtos/create-product.dto.ts
@@ -1,21 +1,26 @@
 import { CreateProductUseCase } from '@/products/application/usecases/create-product.usecase';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateProductDto
   implements Omit<CreateProductUseCase.Input, 'user_id'>
 {
+  @ApiProperty({ description: 'Nome do produto' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({ description: 'Descrição do produto' })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({ description: 'Preço do produto' })
   @IsNumber()
   @IsNotEmpty()
   price: number;
 
+  @ApiProperty({ description: 'Estoque do produto' })
   @IsNumber()
   @IsNotEmpty()
   stock: number;
--- src/products/infrastructure/dtos/update-product.dto.ts
+++ src/products/infrastructure/dtos/update-product.dto.ts
@@ -1,21 +1,26 @@
 import { UpdateProductUseCase } from '@/products/application/usecases/update-product.usecase';
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class UpdateProductDto
   implements Omit<UpdateProductUseCase.Input, 'id' | 'user_id'>
 {
+  @ApiProperty({ description: 'Nome do produto' })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiProperty({ description: 'Descrição do produto' })
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiProperty({ description: 'Preço do produto' })
   @IsOptional()
   @IsNumber()
   price?: number;
 
+  @ApiProperty({ description: 'Estoque do produto' })
   @IsOptional()
   @IsNumber()
   stock?: number;
