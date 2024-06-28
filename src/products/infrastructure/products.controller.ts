d76178c9f6fd71d307245d7154515ec200c67f9a
Cassio <cassiojr0108@gmail.com>
2024-06-27 23:45:42 -0300

    Documentation of products with swagger

--- src/products/infrastructure/products.controller.ts
+++ src/products/infrastructure/products.controller.ts
@@ -27,7 +27,9 @@ import { CreateProductDto } from './dtos/create-product.dto';
 import { AuthGuard } from '@/auth/infrastructure/auth.guard';
 import { ListProductsDto } from './dtos/list-products.dto';
 import { UpdateProductDto } from './dtos/update-product.dto';
+import { ApiBearerAuth, ApiResponse, ApiTags, getSchemaPath } from '@nestjs/swagger';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   @Inject(CreateProductUseCase.UseCase)
@@ -56,6 +58,30 @@ export class ProductsController {
     return new ProductCollectionPresenter(output);
   }
 
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 201,
+    schema: {
+      type: 'object',
+      properties: {
+        data: {
+          type: 'object',
+          properties: {
+            id: { type: 'string', format: 'uuid' },
+            name: { type: 'string' },
+            isSeller: { type: 'boolean' },
+            email: { type: 'string', format: 'email' },
+            created_at: { type: 'string', format: 'date-time' },
+            updated_at: { type: 'string', format: 'date-time' },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Usuário não é vendedor',
+  })
   @UseGuards(AuthGuard)
   @Post()
   async create(
@@ -70,18 +96,108 @@ export class ProductsController {
     return ProductsController.productToResponse(output);
   }
 
+  @ApiResponse({
+    status: 200,
+    schema: {
+      type: 'object',
+      properties: {
+        meta: {
+          type: 'object',
+          properties: {
+            total: {
+              type: 'number',
+            },
+            currentPage: {
+              type: 'number',
+            },
+            lastPage: {
+              type: 'number',
+            },
+            perPage: {
+              type: 'number',
+            },
+          },
+        },
+        data: {
+          type: 'array',
+          items: { $ref: getSchemaPath(ProductPresenter) },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 422,
+    description: 'Parâmetros de consulta inválidos',
+  })
   @Get()
   async search(@Query() searchParams: ListProductsDto) {
     const ouput = await this.listProductsUseCase.execute(searchParams);
     return ProductsController.listProductsToResponse(ouput);
   }
 
+  @ApiResponse({
+    status: 200,
+    schema: {
+      type: 'object',
+      properties: {
+        data: {
+          type: 'object',
+          properties: {
+            id: { type: 'string', format: 'uuid' },
+            name: { type: 'string' },
+            price: { type: 'number' },
+            stock: { type: 'number' },
+            user_id: { type: 'string', format: 'uuid' },
+            created_at: { type: 'string', format: 'date-time' },
+            updated_at: { type: 'string', format: 'date-time' },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Id não encontrado',
+  })
   @Get(':id')
   async findOne(@Param('id') id: string) {
     const output = await this.getProductUseCase.execute({ id });
     return ProductsController.productToResponse(output);
   }
 
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    schema: {
+      type: 'object',
+      properties: {
+        data: {
+          type: 'object',
+          properties: {
+            id: { type: 'string', format: 'uuid' },
+            name: { type: 'string' },
+            price: { type: 'number' },
+            stock: { type: 'number' },
+            user_id: { type: 'string', format: 'uuid' },
+            created_at: { type: 'string', format: 'date-time' },
+            updated_at: { type: 'string', format: 'date-time' },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 422,
+    description: 'Corpo da requisição com dados inválidos',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Id não encontrado',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Acesso não autorizado',
+  })
   @UseGuards(AuthGuard)
   @Put(':id')
   async update(
@@ -98,6 +214,19 @@ export class ProductsController {
     return ProductsController.productToResponse(output);
   }
 
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 204,
+    description: 'Resposta de confirmação da exclusão',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Id não encontrado',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Acesso não autorizado',
+  })
   @UseGuards(AuthGuard)
   @HttpCode(204)
   @Delete(':id')
