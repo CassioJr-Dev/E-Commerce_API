bb34a663a10f6cdb1400e1bc4a7883da5cd47460
Cassio <cassiojr0108@gmail.com>
2024-07-01 11:10:03 -0300

    Create CartEntity

--- src/cart/domain/entities/cart.entity.ts
+++ src/cart/domain/entities/cart.entity.ts
@@ -1,1 +1,24 @@
-export class Cart {}
+import { Entity } from "@/shared/domain/entities/entity";
+
+export type CartProps = {
+  user_id: string
+};
+
+export class CartEntity extends Entity<CartProps>{
+  constructor(
+    public readonly props: CartProps,
+    id?: string,
+    created_at?: Date,
+    updated_at?: Date,
+  ){
+    super(props, id, created_at, updated_at);
+  }
+
+  get user_id() {
+    return this.props.user_id
+  }
+
+  set user_id(value: string) {
+    this.props.user_id = value
+  }
+}
