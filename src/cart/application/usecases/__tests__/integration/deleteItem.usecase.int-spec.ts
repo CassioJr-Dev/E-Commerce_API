89aa0d91546f00232d0642dbb38afc8fc62fdd81
Cassio <cassiojr0108@gmail.com>
2024-07-19 00:07:26 -0300

    DeleteItemUseCase integration tests

--- src/cart/application/usecases/__tests__/integration/deleteItem.usecase.int-spec.ts
+++ src/cart/application/usecases/__tests__/integration/deleteItem.usecase.int-spec.ts
@@ -8,13 +8,19 @@ import { UserEntity } from '@/users/domain/entities/user.entity';
 import { CartPrismaRepository } from '@/cart/infrastructure/database/prisma/repositories/cart-prisma.repository';
 import { CartEntity } from '@/cart/domain/entities/cart.entity';
 import { DeleteCartUseCase } from '../../deleteCart.usecase';
+import { DeleteItemUseCase } from '../../deleteItem.usecase';
+import { ProductEntity } from '@/products/domain/entities/product.entity';
+import { ProductDataBuilder } from '@/products/domain/testing/helpers/product-data-builder';
+import { CartItemEntity } from '@/cart/domain/entities/cartItem.entity';
 
-describe('DeleteCartUseCase integration tests', () => {
+describe('DeleteItemUseCase integration tests', () => {
   const prismaService = new PrismaClient();
-  let sut: DeleteCartUseCase.UseCase;
+  let sut: DeleteItemUseCase.UseCase;
   let repository: CartPrismaRepository;
   let module: TestingModule;
   let user: UserEntity;
+  let cart: CartEntity
+  let product: ProductEntity
 
   beforeAll(async () => {
     setupPrismaTests();
@@ -26,14 +32,24 @@ describe('DeleteCartUseCase integration tests', () => {
   });
 
   beforeEach(async () => {
-    sut = new DeleteCartUseCase.UseCase(repository);
+    sut = new DeleteItemUseCase.UseCase(repository);
     await prismaService.cart.deleteMany();
     await prismaService.cartItem.deleteMany();
     await prismaService.user.deleteMany();
 
     user = new UserEntity({ ...UserDataBuilder({}), isSeller: true });
-
     await prismaService.user.create({ data: user });
+
+    cart = new CartEntity({ user_id: user.id });
+    await prismaService.cart.create({
+      data: cart,
+    });
+
+    product = new ProductEntity({
+      ...ProductDataBuilder({}),
+      user_id: user.id,
+    });
+    await prismaService.product.create({ data: product });
   });
 
   afterAll(async () => {
@@ -48,23 +64,31 @@ describe('DeleteCartUseCase integration tests', () => {
     const newUser = new UserEntity({ ...UserDataBuilder({}), isSeller: true });
     await prismaService.user.create({ data: newUser });
 
+
     await expect(() =>
-      sut.execute({ cart_id: 'fakeId', user_id: user.id }),
+      sut.execute({ item_id: 'fakeID', cart_id: 'fakeID', user_id: newUser.id }),
     ).rejects.toThrow(new NotFoundError('Cart not found'));
   });
 
-  it('Should delete a cart', async () => {
-    const cartEntity = new CartEntity({ user_id: user.id });
-    await prismaService.cart.create({
-      data: cartEntity,
-    });
+  it('Should delete a item to cart', async () => {
+    const newItem = new CartItemEntity({ cart_id: cart.id, product_id: product.id, quantity: 10 })
+    await prismaService.cartItem.create({
+      data: newItem
+    })
 
     const output = await sut.execute({
-      cart_id: cartEntity.id,
-      user_id: user.id,
+      item_id: newItem.id, cart_id: cart.id, user_id: user.id
     });
+
     expect(output).toBeUndefined();
-    const models = await prismaService.cart.findMany();
+    const models = await prismaService.cartItem.findMany();
     expect(models).toHaveLength(0);
+  });
+
+  it('Should delete a item to cart not exists', async () => {
+
+    await expect(() =>
+      sut.execute({ item_id: 'fakeID', cart_id: cart.id, user_id: user.id }),
+    ).rejects.toThrow(new NotFoundError('Item not found'));
   });
 });
